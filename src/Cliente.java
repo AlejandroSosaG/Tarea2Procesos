2d4f9d5ff6ff970668f1a6c78876da284dda559b
AlejandroSosaG <alesosagar@gmail.com>
2023-02-02 11:54:47 +0100

    Tarea Finalizada

---
+++ src/Cliente.java
@@ -0,0 +1,38 @@
+import java.io.IOException;
+import java.net.*;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Cliente {
+    public static void main(String[] args) {
+        Random r = new Random();
+        //  Obtener dirección IP local
+        InetAddress direccion = null;
+        try {
+            direccion = InetAddress.getLocalHost();
+            // Creación del socket
+            DatagramSocket socket = new DatagramSocket();
+            //  Creación del mensaje
+            int numeroMensaje = 0;
+            String mensaje;
+            while (numeroMensaje<10000){
+                mensaje = "Mensaje: " + numeroMensaje;
+                byte[] buffer = mensaje.getBytes();
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length, direccion, 42000);
+                socket.send(packet);
+                numeroMensaje++;
+            }
+            mensaje = "FIN";
+            byte[] buffer = mensaje.getBytes();
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length, direccion, 42000);
+            socket.send(packet);
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        } catch (SocketException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
---
+++ src/Servidor.java
@@ -0,0 +1,36 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
+public class Servidor {
+    public static void main(String[] args) {
+        try{
+            DatagramSocket socket = new DatagramSocket(42000);
+            System.out.println("Creación del arra de bytes");
+            System.out.println("A la espera de recibir datagrama");
+            String mensaje;
+            File fichero = new File("mensajes.txt");
+            FileWriter fw = new FileWriter("mensajes.txt");
+            fichero.createNewFile();
+            while(true) {
+                byte[] buffer = new byte[64];
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+                socket.receive(packet);
+                mensaje = new String(packet.getData()).trim();
+                if (mensaje.equals("FIN")) break;
+                fw.write(mensaje.substring(9) + " ");
+                System.out.println(mensaje.substring(9).trim() + " ");
+            }
+            fw.close();
+        } catch (SocketException e) {
+            System.err.println("Error en la creación del socket");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.err.println("Error en la recepción del paquete");
+            e.printStackTrace();
+        }
+    }
+}
